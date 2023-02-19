92543a9571de49a9ff165fb671aa3c482da455ab
gamma916 <garfeildma@yandex.com>
2023-02-19 20:10:32 +0800

    airdrop to owner when add

--- src/main/kotlin/com/tbook/rewardsphere/controller/HomeController.kt
+++ src/main/kotlin/com/tbook/rewardsphere/controller/HomeController.kt
@@ -1,5 +1,6 @@
 package com.tbook.rewardsphere.controller
 
+import com.tbook.rewardsphere.auth.AddressAuthentication
 import com.tbook.rewardsphere.model.Attribute
 import com.tbook.rewardsphere.model.NFTMetaData
 import com.tbook.rewardsphere.service.ContractService
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 import java.net.URI
+import java.security.Principal
 
 @RestController
 class HomeController(
@@ -47,7 +49,9 @@ class HomeController(
     }
 
     @PostMapping("/addMapping")
-    fun deployContract(@RequestParam("twitId") twitId: String): Any {
+    fun deployContract(@RequestParam("twitId") twitId: String,
+                       @RequestParam("address", required = false, defaultValue = "") address: String,
+                       principal: Principal): Any {
         try {
             val pic = "https://ik.imagekit.io/tweetpik/356884789962211917/${twitId}.png"
             //val content = twitterService.getTweet(twitId, twitterToken)
@@ -57,6 +61,12 @@ class HomeController(
             val nftId = contractService.addMapping(twitId)
             s3Service.putMeta(nftId.toString(), meta)
             //redisTemplate.opsForValue().set(twitId, nftId)
+            val authentication = principal as AddressAuthentication?
+            if (authentication != null) {
+                contractService.airDropTo(authentication.address, nftId, 1)
+            } else if (address.isNotEmpty() && address != "undefined") {
+                contractService.airDropTo(address, nftId, 1)
+            }
             return mapOf("opensea" to "https://testnets.opensea.io/assets/mumbai/0xACdE17C1A595Ae2Cf12605a157Ae8Ad5Ddf8953F/${nftId}",
                     "contract" to "https://mumbai.polygonscan.com/address/0xACdE17C1A595Ae2Cf12605a157Ae8Ad5Ddf8953F",
                     "nftId" to nftId
